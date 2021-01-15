26537c258b84b719e5472f4e3ff7d81ab87c4364
yukilost <yukilost42@gmail.com>
2021-01-16 01:14:08 +0900

    Make a number compilable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Incorrect number of arguents");
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", &args[1].parse::<i32>().expect("Not a number"));
+    println!("  ret");
 }
